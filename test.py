c9298cd1a114ed0fda61dd51ffa5bcd8208b5913
unknown <max.oblein@gmail.com>
2020-10-25 20:44:57 +0000

    get coordinates and plot

--- test.py
+++ test.py
@@ -11,3 +11,18 @@ for feature in data['features']:
         print(feature['geometry']['type'])
         print(feature['geometry']['coordinates'])
         vector = feature['geometry']['coordinates']
+
+xcoord = np.ones(len(vector))
+ycoord = np.ones(len(vector))
+
+for i in range(len(vector)):
+    xcoord[i] = vector[i][0]
+    ycoord[i] = vector[i][1]
+
+
+print(xcoord)
+print(ycoord)
+#fig = plt.figure()
+#ax =  fig.add_subplot(111)
+#ax.plot(xcoord,ycoord)
+#plt.show()
