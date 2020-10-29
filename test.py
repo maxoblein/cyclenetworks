dfd105521b76f2a025f1694cca0e1ef4a658a77c
unknown <max.oblein@gmail.com>
2020-10-29 11:49:28 +0000

    added distance code

--- test.py
+++ test.py
@@ -6,25 +6,22 @@ import matplotlib.pyplot as plt
 with open('bristolcycle.geojson') as f:
     data = json.load(f)
 
+ways = []
+
+
 for feature in data['features']:
-    if feature['id'] == 'way/484111801':
-        print(feature['geometry']['type'])
-        print(feature['geometry']['coordinates'])
-        vector = feature['geometry']['coordinates']
+    vector = feature['geometry']['coordinates']
+    ways.append(vector)
 
-xcoord = np.ones(len(vector))
-ycoord = np.ones(len(vector))
-
-for i in range(len(vector)):
-    xcoord[i] = vector[i][0]
-    ycoord[i] = vector[i][1]
+print(len(ways))
 
 
-print(xcoord)
-print(ycoord)
+#distance between latlong
+'''
+import geopy.distance
 
+coords_1 = (52.2296756, 21.0122287)
+coords_2 = (52.406374, 16.9251681)
 
-fig = plt.figure()
-ax =  fig.add_subplot(111)
-ax.scatter(xcoord,ycoord)
-plt.show()
+print geopy.distance.vincenty(coords_1, coords_2).km
+'''
