db14e06f01c8c89c1522bd869ad5fe367c05400a
unknown <max.oblein@gmail.com>
2021-02-09 10:47:51 +0000

    graphs for eddie

--- edgeflows.py
+++ edgeflows.py
@@ -30,14 +30,11 @@ def getflows(adjusted,nodes,flowmat,ntrips):
     start = timer()
     #loop for however many iterations to get flows
     for i in range(ntrips):
-        print('new path')
         path, ecpath, pct_cycle, length = random_shortest_path(adjusted,ODoption = 'lsoa')
 
         flowmat = updatemat(path,nodes,flowmat)
 
 
-    print(np.nonzero(flowmat))
-    print(np.amax(flowmat))
     end = timer()
     print('update time = ',(end-start))
 
@@ -50,17 +47,15 @@ def largest_indices(ary, n):
     indices = indices[np.argsort(-flat[indices])]
     return np.unravel_index(indices, ary.shape)
 
-def upgraderoads(G,flowmat,cycmat,batchsize = 1,batchno=1):
+def upgraderoads(G,flowmat,cycmat,updated,batchsize = 1,batchno=1):
     nodes = list(G.nodes)
     edges = list(G.edges)
     inds = largest_indices(flowmat,flowmat.shape[0])
-    print(inds)
-    print(nodes[inds[1][0]])
     edges = []
     for i in range(flowmat.shape[0]):
          edges.append([nodes[inds[0][i]],nodes[inds[1][i]]])
-    print(edges)
     indicator = 0
+
     j = 0
     while indicator != batchsize:
 
@@ -71,17 +66,15 @@ def upgraderoads(G,flowmat,cycmat,batchsize = 1,batchno=1):
 
 
         else:
-            print(G.edges[(edges[j][0],edges[j][1],0)])
             G.edges[(edges[j][0],edges[j][1],0)]['highway'] = 'cycleway'
             cycmat[nodes.index(edges[j][0])][nodes.index(edges[j][1])] = batchno
-            print(G.edges[(edges[j][0],edges[j][1],0)])
-            print(j)
+            updated.append([G.edges[(edges[j][0],edges[j][1],0)],batchno])
             indicator = indicator + 1
 
         j = j+1
 
 
-    return G
+    return G, updated
 
 
 
@@ -93,19 +86,20 @@ if __name__ == '__main__':
 
     print('no. cycle paths = ',ec.count('r'))
 
-    #for plotting highlighted graph
-    fig, ax = ox.plot_graph(G_copy, node_size=0, edge_color=ec, edge_linewidth=1.5, edge_alpha=0.7,show = False)
-    ax.set_title('Graph of road network in Bristol with cycle paths highlighted', fontsize = 18)
-
-
-
-    red_patch = mpatches.Patch(color='red', label='Roads with cycle paths')
-
-    ax.legend(handles=[red_patch])
-
-    plt.show()
+    # #for plotting highlighted graph
+    # fig, ax = ox.plot_graph(G_copy, node_size=0, edge_color=ec, edge_linewidth=1.5, edge_alpha=0.7,show = False)
+    # ax.set_title('Graph of road network in Bristol with cycle paths highlighted', fontsize = 18)
+    #
+    #
+    #
+    # red_patch = mpatches.Patch(color='red', label='Roads with cycle paths')
+    #
+    # ax.legend(handles=[red_patch])
+    #
+    # plt.show()
 
     batchsize = 50
+    updated = []
 # test for a few batches
     for batchno in range(10):
         adjusted,cycmat = adjust_weights(G_copy,25)
@@ -114,7 +108,7 @@ if __name__ == '__main__':
 
         flowmat = getflows(adjusted,nodes,flowmat,ntrips)
 
-        G_next = upgraderoads(G_copy,flowmat,cycmat,batchsize,batchno+1)
+        G_next,updated = upgraderoads(G_copy,flowmat,cycmat,updated,batchsize,batchno+1)
 
         G_copy = G_next
 
@@ -122,17 +116,19 @@ if __name__ == '__main__':
 
     ec = colour_edges(G_next)
 
-    print(cycmat)
+    print(updated)
     print('no. cycle paths = ',ec.count('r'))
 
-    #for plotting highlighted graph
-    fig, ax = ox.plot_graph(G_next, node_size=0, edge_color=ec, edge_linewidth=1.5, edge_alpha=0.7,show = False)
-    ax.set_title('Graph of road network in Bristol with cycle paths highlighted', fontsize = 18)
 
 
-
-    red_patch = mpatches.Patch(color='red', label='Roads with cycle paths')
-
-    ax.legend(handles=[red_patch])
-
-    plt.show()
+    # #for plotting highlighted graph
+    # fig, ax = ox.plot_graph(G_next, node_size=0, edge_color=ec, edge_linewidth=1.5, edge_alpha=0.7,show = False)
+    # ax.set_title('Graph of road network in Bristol with cycle paths highlighted', fontsize = 18)
+    #
+    #
+    #
+    # red_patch = mpatches.Patch(color='red', label='Roads with cycle paths')
+    #
+    # ax.legend(handles=[red_patch])
+    #
+    # plt.show()
